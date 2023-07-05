a87fd51ad1e2bb1d67c74396505255308be5fa8e
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2023-07-05 13:23:50 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,17 @@
+// module.exports = {
+
+//   //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+//   //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+//    //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+// url:"mongodb+srv://Blessing:KF4CzZpj8tlYGtap@cluster0.29680fe.mongodb.net/SportStore?retryWrites=true&w=majority"
+
+// };
+
+module.exports = {
+  url: "mongodb+srv://tatianakawaguti:eB5Oy3dmsUQrMC3S@cluster0.rr9pz6y.mongodb.net/DressStore?retryWrites=true&w=majority"
+};
